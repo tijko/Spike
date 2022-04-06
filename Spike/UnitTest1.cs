3412d9a1532a1959ce2368ec6afa4f5c425ae68e
tim konick <tkonick@nedelta.com>
2022-04-06 15:25:44 -0400

    Added unittests for "Theory"

--- Spike/UnitTest1.cs
+++ Spike/UnitTest1.cs
@@ -5,9 +5,34 @@ namespace Spike
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void PassingTest()
         {
+            Assert.Equal(4, Add(2, 2));
+        }
 
+        [Fact]
+        public void FailingTest()
+        {
+            Assert.Equal(5, Add(3, 2));
+        }
+
+        [Theory]
+        [InlineData(3)]
+        [InlineData(5)]
+        [InlineData(7)]
+        public void MyFirstTheory(int value)
+        {
+            Assert.True(IsOdd(value));
+        }
+
+        bool IsOdd(int value)
+        {
+            return value % 2 == 1;
+        }
+
+        int Add(int x, int y)
+        { 
+            return x + y; 
         }
     }
 }
